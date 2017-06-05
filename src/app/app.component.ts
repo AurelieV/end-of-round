be3f221c9eb299cf1089851e1bcac43597b10a3e
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-06-05 23:47:59 +0200

    Add status and legend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MdDialog } from '@angular/material';
 
 import { Tournament, Zone } from './model';
 import { TournamentService } from './services/tournament.service';
@@ -14,11 +15,15 @@ interface ZoneInformation {
 })
 export class AppComponent implements OnInit {
   tournament: Tournament;
+  @ViewChild('help') help: TemplateRef<any>;
 
-  constructor(private tournamentService: TournamentService) {}
+  constructor(private tournamentService: TournamentService, private md: MdDialog) {}
 
   ngOnInit() {
     this.tournamentService.getTournament().subscribe(tournament => this.tournament = tournament);
   }
 
+  openHelp() {
+    this.md.open(this.help);
+  }
 }
