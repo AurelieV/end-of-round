0793dbead3bd4ae232ce2b86f5776bee24cc90e6
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-06-07 00:19:11 +0200

    Display true stat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, OnDestroy } from '@angular/core';
 import { MdDialog } from '@angular/material';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Zone } from './model';
 
@@ -13,17 +14,31 @@ interface ZoneInformation {
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   zones$: FirebaseListObservable<Zone[]>;
   @ViewChild('help') help: TemplateRef<any>;
+  playingTableNumber: number;
+  coveredTableNumber: number;
+  extraTimedTableNumber: number;
+
+  private subscriptions: Subscription[] = [];
 
   constructor(private md: MdDialog, private db: AngularFireDatabase) {}
 
   ngOnInit() {
     this.zones$ = this.db.list('/vegas/zones');
+    this.subscriptions.push(this.db.object('/vegas/tables').subscribe(tables => {
+        this.playingTableNumber = tables.filter(t => t.status === "playing").length;
+        this.coveredTableNumber = tables.filter(t => t.status === "covered").length;
+        this.extraTimedTableNumber = tables.filter(t => t.time > 0).length;
+    }));
   }
 
   openHelp() {
     this.md.open(this.help);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 }
