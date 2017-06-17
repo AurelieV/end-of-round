bb16c6863ece24c7d749733618befb95baf5b56a
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-06-17 09:58:52 +0200

    Update display of result

--- src/app/pipes/result.ts
+++ src/app/pipes/result.ts
@@ -7,12 +7,16 @@ export class ResultPipe implements PipeTransform {
 
     transform(value: Result): string {
         if (!value) return "";
-        let result = `${value.player1.score}-${value.draw}-${value.player2.score}`;
+        let result = "";
         if (value.player1.drop) {
-            result += ' P1 drop';
+            result += 'X-';
+        }
+        result += `${value.player1.score}-${value.player2.score}`;
+        if (value.draw > 0) {
+            result += "-d"
         }
         if (value.player2.drop) {
-            result += ' P2 drop';
+            result += '-X';
         }
 
         return result;
