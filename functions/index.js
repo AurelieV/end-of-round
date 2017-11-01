587d348aeed89838ab4abc736327187fac67e33a
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-11-01 17:01:49 +0100

    Add clound function for delete tournament

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const tournamentFields = ['zones', 'messages', 'outstandings', 'tables' ];
+exports.deleteTournament = functions.database.ref('/tournaments/{id}')
+  .onDelete(event => {
+    const id = event.params.id;
+    return Promise.all(tournamentFields.map(field => {
+      return admin.database().ref(`/${field}/${id}`).remove()
+    }))
+  })
+
