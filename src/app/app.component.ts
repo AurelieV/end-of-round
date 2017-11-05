bc2f7423e0a93d6d3c06c30a6192c44a67159067
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-11-06 00:11:52 +0100

    Maxlength for tournamentLength + factorize toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ interface State {
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
   state: State = {
