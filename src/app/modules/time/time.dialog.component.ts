da4c41c632a28b2a17187fdb2b84f38bd8050143
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-12-09 15:39:04 +0100

    Add all tables time possibility

--- src/app/modules/time/time.dialog.component.ts
+++ src/app/modules/time/time.dialog.component.ts
@@ -10,7 +10,7 @@ import { FormControl } from '@angular/forms';
 interface TimeData {
     time: number;
     tableNumber: string;
-    seat: 'A' | 'B' | 'C';
+    seat: 'A' | 'B' | 'C' | 'all';
 }
 
 @Component({
@@ -44,7 +44,7 @@ export class TimeDialogComponent implements OnInit {
             return table.number !== '' + tableId;
         });
         this.displayMessage = Observable.combineLatest(this.isLoading, this.currentTable$).map(([isLoading, table]) => {
-            return !isLoading && table && 
+            return !isLoading && table && this.data.seat !== 'all' &&
                 ((!this.isTeam && table.time > 0) || (this.isTeam && table.time[this.data.seat] > 0));
         });
     }
@@ -55,15 +55,24 @@ export class TimeDialogComponent implements OnInit {
 
     onSeatChange(value) {
         this.currentTableId$.take(1).subscribe(id => this.currentTableId$.next(id));
+        if (value === 'all') {
+            this.addOrUpdate = 'add';
+        }
     }
 
     submit(wantToContinue?: boolean) {
         this.currentTable$.take(1).subscribe(table => {
             let time = this.data.time;
-            if (table && this.addOrUpdate === 'add' && table.time) {
+            if (table && this.data.seat !== 'all' && this.addOrUpdate === 'add' && table.time) {
                 time += this.isTeam ? (table.time[this.data.seat] || 0) : table.time;
             }
-            this.tournamentService.setTime(time, this.data.tableNumber, this.isTeam ? this.data.seat : null);
+            if (this.data.seat !== 'all') {
+                this.tournamentService.setTime(time, this.data.tableNumber, this.isTeam ? this.data.seat : null);
+            } else {
+                this.tournamentService.setTime(this.data.time + (table.time['A'] || 0), this.data.tableNumber, 'A');
+                this.tournamentService.setTime(this.data.time + (table.time['B'] || 0), this.data.tableNumber,'B');
+                this.tournamentService.setTime(this.data.time + (table.time['C'] || 0), this.data.tableNumber,'C');
+            }
             if (wantToContinue) {
                 this.form.reset();
                 setTimeout(() => {
